5c3aceff667818f98566aac0f2736a992e2952b0
lahcen Lamnaï <lahcenlamnai3@gmail.com>
2024-03-08 22:22:18 +0100

    la solution de EX3

--- src/TP3/EX3.java
+++ src/TP3/EX3.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 public class EX3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         /*Exercice 3 :
         Écrire un programme qui lit la taille N d'un tableau T du type int (dimension maximale:
         50 vérifier la taille et demandez à l’utilisateur de saisir la taille tant que la valeur n’est pas comprise entre 10 et 50),
         remplit le tableau par des valeurs entrées au clavier et l’afficher.
-         */
+
+        */
        int taille;
         do{
             System.out.println("enter la taille d'un tableau: ");
@@ -24,11 +26,5 @@ public class EX3 {
        for (int i=0;i< T.length;i++) {
             System.out.print(T[i] + " ");
         }
-        System.out.println(" 11");
-        Arrays.fill( T ,0,5,0);
-        for (int i=0;i< T.length;i++) {
-            System.out.print(T[i] + " ");
-        }
-
     }
 }
